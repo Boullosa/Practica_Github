25114e4bf2615c5261d04c41c19d4e84e254712f
dam116 <dam116@pc2029.aulas.local>
2016-05-23 11:55:56 +0200

    Segunda confirmación

--- src/nifcontrol/NIFcontrol.java
+++ src/nifcontrol/NIFcontrol.java
@@ -13,20 +13,6 @@ public class NIFcontrol {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Lector in = new Lector();
-        boolean fallo;
-        NIF n;
-        
-        do{
-            try{
-                System.out.print("Introduce tu nif: ");
-                n = new NIF(in.leerCadena());
-                System.out.println("NIF: "+n.getNif());
-                fallo=false;
-            }catch(NifLetraException | NifLetraExisteException | NifDigitosException | NifTamanoException e){
-                System.err.println(e.getMessage());
-                fallo=true;
-            }
-        }while(fallo);
+        System.out.println("Esto no hace nada");
     }
 }
